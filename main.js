577cc787fad42e94fdd9f8123aa2c8edc6a7c78b
EmanueleRangel <emanuele.rangel52@gmail.com>
2022-03-21 13:05:01 -0300

    forms improved

--- main.js
+++ main.js
@@ -12,16 +12,20 @@ botao.addEventListener("click", (event) => {
     const reg = /^([A-Za-z0-9\.]{1,32})\@([a-z0-9]{1,16})\.([A-Za-z]{2,4})$/;
 
     if (texto.value == ""){
-
         var mensagem = document.getElementById("msgEmail");
         mensagem.innerHTML="Erro no envio: Insira uma mensagem";
          }
         else if(reg.test(email.value) == false){
             var mensagem = document.getElementById("msgEmail");
             mensagem.innerHTML="Erro no envio: Endereço de mail inválido";
+
+            document.getElementById("email").value="";
         } else {
             var mensagem = document.getElementById("msgEmail");
             mensagem.innerHTML=("Obrigada pelo contato, " + usuario+" !");
+
+            document.getElementById("email").value="";
+            document.getElementById("texto").value="";
         }
     }
 )
